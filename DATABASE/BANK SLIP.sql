e995c584cc31b61f21e1099e4f1749ed01a2e2d9
pratik37 <110971130+pratik37@users.noreply.github.com>
2022-09-08 15:36:53 +0530

    Add files via upload

---
+++ DATABASE/BANK SLIP.sql
@@ -0,0 +1,33 @@
+--------------------------------------------------------
+--  File created - Thursday-September-08-2022   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table BANK_SLIP_DETAILS
+--------------------------------------------------------
+
+  CREATE TABLE "BANK_SLIP_DETAILS" ("SLIP_NO" VARCHAR2(10), "SLIP_DATE" DATE, "ACCOUNT_NO" VARCHAR2(25), "CHEQUE_NO" VARCHAR2(25), "PAYER_ACCOUNT_ID" VARCHAR2(30), "AMOUNT" NUMBER(10,0))
+REM INSERTING into BANK_SLIP_DETAILS
+SET DEFINE OFF;
+Insert into BANK_SLIP_DETAILS (SLIP_NO,SLIP_DATE,ACCOUNT_NO,CHEQUE_NO,PAYER_ACCOUNT_ID,AMOUNT) values ('1000',to_date('07-SEP-22','DD-MON-RR'),'AC79220000','1022','AC79220002',5000);
+Insert into BANK_SLIP_DETAILS (SLIP_NO,SLIP_DATE,ACCOUNT_NO,CHEQUE_NO,PAYER_ACCOUNT_ID,AMOUNT) values ('1001',to_date('07-SEP-22','DD-MON-RR'),'AC79220001','1023','AC79220003',15000);
+Insert into BANK_SLIP_DETAILS (SLIP_NO,SLIP_DATE,ACCOUNT_NO,CHEQUE_NO,PAYER_ACCOUNT_ID,AMOUNT) values ('1002',to_date('07-SEP-22','DD-MON-RR'),'AC79220002','1021','AC79220001',51000);
+Insert into BANK_SLIP_DETAILS (SLIP_NO,SLIP_DATE,ACCOUNT_NO,CHEQUE_NO,PAYER_ACCOUNT_ID,AMOUNT) values ('1003',to_date('07-SEP-22','DD-MON-RR'),'AC79220003','1024','AC79220004',5900);
+Insert into BANK_SLIP_DETAILS (SLIP_NO,SLIP_DATE,ACCOUNT_NO,CHEQUE_NO,PAYER_ACCOUNT_ID,AMOUNT) values ('1004',to_date('07-SEP-22','DD-MON-RR'),'AC79220004','1020','AC79220000',7000);
+--------------------------------------------------------
+--  DDL for Index SLIP_NO_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "SLIP_NO_PK" ON "BANK_SLIP_DETAILS" ("SLIP_NO")
+--------------------------------------------------------
+--  Constraints for Table BANK_SLIP_DETAILS
+--------------------------------------------------------
+
+  ALTER TABLE "BANK_SLIP_DETAILS" MODIFY ("SLIP_NO" NOT NULL ENABLE)
+  ALTER TABLE "BANK_SLIP_DETAILS" MODIFY ("AMOUNT" NOT NULL ENABLE)
+  ALTER TABLE "BANK_SLIP_DETAILS" ADD CONSTRAINT "SLIP_NO_PK" PRIMARY KEY ("SLIP_NO") USING INDEX  ENABLE
+--------------------------------------------------------
+--  Ref Constraints for Table BANK_SLIP_DETAILS
+--------------------------------------------------------
+
+  ALTER TABLE "BANK_SLIP_DETAILS" ADD CONSTRAINT "SLIP_ACCOUNT_NO_FK" FOREIGN KEY ("ACCOUNT_NO") REFERENCES "ACCOUNT_DETAILS" ("ACCOUNT_NO") ENABLE
+  ALTER TABLE "BANK_SLIP_DETAILS" ADD CONSTRAINT "SLIP_CHEQUE_NO_FK" FOREIGN KEY ("CHEQUE_NO") REFERENCES "CHEQUE_DETAILS" ("CHEQUE_NO") ENABLE
---
+++ DATABASE/LOGIN DETAILS.sql
@@ -0,0 +1,28 @@
+--------------------------------------------------------
+--  File created - Thursday-September-08-2022   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table LOGIN_DETAILS
+--------------------------------------------------------
+
+  CREATE TABLE "LOGIN_DETAILS" ("LOGIN_ID" VARCHAR2(20), "USERNAME" VARCHAR2(20), "PASSWORD" VARCHAR2(20), "DESIGNATION" VARCHAR2(10))
+REM INSERTING into LOGIN_DETAILS
+SET DEFINE OFF;
+Insert into LOGIN_DETAILS (LOGIN_ID,USERNAME,PASSWORD,DESIGNATION) values ('INB1000','PRATIK','Pratik@123','Admin');
+Insert into LOGIN_DETAILS (LOGIN_ID,USERNAME,PASSWORD,DESIGNATION) values ('INB1001','VINNET','Vineet@123','Admin');
+Insert into LOGIN_DETAILS (LOGIN_ID,USERNAME,PASSWORD,DESIGNATION) values ('INB1002','PRADIP','Pradip@123','Admin');
+Insert into LOGIN_DETAILS (LOGIN_ID,USERNAME,PASSWORD,DESIGNATION) values ('INB1003','SURAJ','Suraj@123','Admin');
+--------------------------------------------------------
+--  DDL for Index LOGIN_ID_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "LOGIN_ID_PK" ON "LOGIN_DETAILS" ("LOGIN_ID")
+--------------------------------------------------------
+--  Constraints for Table LOGIN_DETAILS
+--------------------------------------------------------
+
+  ALTER TABLE "LOGIN_DETAILS" MODIFY ("LOGIN_ID" NOT NULL ENABLE)
+  ALTER TABLE "LOGIN_DETAILS" MODIFY ("USERNAME" NOT NULL ENABLE)
+  ALTER TABLE "LOGIN_DETAILS" MODIFY ("PASSWORD" NOT NULL ENABLE)
+  ALTER TABLE "LOGIN_DETAILS" MODIFY ("DESIGNATION" NOT NULL ENABLE)
+  ALTER TABLE "LOGIN_DETAILS" ADD CONSTRAINT "LOGIN_ID_PK" PRIMARY KEY ("LOGIN_ID") USING INDEX  ENABLE
